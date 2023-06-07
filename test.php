e149ae818355b44337cf7755f258ba47160234b1
Bishal <106469360+bishal959@users.noreply.github.com>
2023-06-07 14:59:31 +0545

    s

--- test.php
+++ test.php
@@ -24,7 +24,7 @@ include("teacher_header.php")
 <title>Attendance Management System</title>
 <!-- <link rel="stylesheet" href="form.css" /> -->
     <link rel="icon" href="icon.png">
-    <!-- <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet"> -->
+    <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://kit.fontawesome.com/385a42cb55.js" crossorigin="anonymous" defer="">
     </script>
 </head>
