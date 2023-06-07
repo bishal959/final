8bf2282b18b9c33a92a367d5420f010673f33441
Bishal <106469360+bishal959@users.noreply.github.com>
2023-06-07 15:09:04 +0545

    s

--- atenpost.php
+++ atenpost.php
@@ -15,23 +15,23 @@ if (!$can_access_page) {
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $subject = $_POST['subject'];
     $date = $_POST['date'];
-    $attendance_status = $_POST['attendance_status'];
+    $status = $_POST['status'];
 
     // Check if any user attendance is selected
-    if (isset($_POST['attendance_status']) && !empty($_POST['attendance_status'])) {
-        $userAttendances = $_POST['attendance_status'];
+    if (isset($_POST['status']) && !empty($_POST['status'])) {
+        $userAttendances = $_POST['status'];
 
         // Iterate through the selected user attendances
         foreach ($userAttendances as $user => $status) {
             // Perform the necessary database operations to save the attendance record
             // For example, you can execute an SQL query to insert the attendance record into the database
-            $sql = "INSERT INTO attendance (user_name, subject, date, attendance_status) 
+            $sql = "INSERT INTO attendance (user_name, subject, date, status) 
                     VALUES ('$user', '$subject', '$date', '$status')";
             mysqli_query($conn, $sql);
         }
 
         // Redirect to a success page or perform additional actions
-        header('location: success.php');
+        header('location: teacher.php');
         exit();
     }
 }
