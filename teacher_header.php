b27e40bee3a5cc3a72bb7a3a4e9f207d5fee2b35
Bishal <106469360+bishal959@users.noreply.github.com>
2023-06-07 15:38:31 +0545

    f

--- teacher_header.php
+++ teacher_header.php
@@ -12,7 +12,7 @@
             <a href="html.php"><h1>Attendance Management System</h1></a>
                 <ul class="header-ul">
                 <li><a href="download.php">download attendance</a></li>
-                <li><a href="teacher.php">Attendance</a></li>
+                <li><a href="test.php">Attendance</a></li>
                 <li><a href="listattendance.php">list attendance</a></li>
                 <li><a href="liststudent.php">list student</a></li>
                 <li><a href="session.php" >Hello, <?php print_r($_SESSION['teacher_name']); ?></a></li>
