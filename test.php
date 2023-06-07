9a46f1b7d87829eb2f7d182e5c83aa8d2926d553
Bishal <106469360+bishal959@users.noreply.github.com>
2023-06-07 14:24:37 +0545

    x

--- test.php
+++ test.php
@@ -1,3 +1,37 @@
+<?php
+session_start();
+include "db_conn.php";
+
+$can_access_page = false;
+if(isset($_SESSION['admin_id'])  || isset($_SESSION['teacher_id'])){
+    $can_access_page = true;
+}
+
+if(!$can_access_page){
+    header('location: index.php');
+    exit();
+}
+
+$sql = "SELECT user_name FROM users WHERE role='user'";
+$result = mysqli_query($conn, $sql);
+
+include("teacher_header.php")
+
+?>
+
+<html>
+<head>
+<title>Attendance Management System</title>
+<!-- <link rel="stylesheet" href="form.css" /> -->
+  <link rel="icon" href="icon.png">
+<script src="https://kit.fontawesome.com/385a42cb55.js" crossorigin="anonymous" defer="">
+  <link rel="stylesheet" href="./sass/style.css">
+  </script>
+</head>
+<body>
+
+<main>
+<h2>Attendance Form</h2>
 <form method="POST" action="attendance.php">
   <table>
     <tr>
@@ -30,3 +64,14 @@
 
   <button type="submit">Submit</button>
 </form>
+
+</main>
+<script>
+    // Get the current date
+    var currentDate = new Date().toISOString().split('T')[0];
+
+    // Set the current date as the default value for the date input field
+    document.getElementById("date").value = currentDate;
+  </script>
+</html>
+<?php include("footer.php");?>
